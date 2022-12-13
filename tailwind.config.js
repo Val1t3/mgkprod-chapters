5b24b146292ca0d968c6071a390d29d2a259a5d8
Val <valentinwoehrel2003@gmail.com>
2022-12-13 14:41:36 +0100

    [ADD] styles to frame-menu + beginning animation list-point

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dmsans': ['DM Sans', 'sans-serif'],
+      },
+      fontSize: {
+        xl: '92px',
+      },
+    },
   },
   plugins: [],
 }
